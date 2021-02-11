ea748d9833bcdb730748a9c3c986eb9687117a28
Luanacvlcnt <lua.cavalcanti19@gmail.com>
2021-02-10 23:17:23 -0300

    commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+function botao () {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigada por clicar</b>";
+
+    //console.log(document.getElementById("agradecimento"));
+}
+
+function redirecionar() {
+    window.open("https://www.linkedin.com/in/luana-cavalcanti-a815a3194/");
+    //window.location.href = "https://www.linkedin.com/in/luana-cavalcanti-a815a3194/";
+}
+
+function trocar (elemento) {
+    //document.getElementById("mouse").innerHTML = "Obrigada por passar o mouse";
+    //alert("trocar texto");
+    elemento.innerHTML = "Obrigada por passar o mouse";
+}
+
+function voltar (elemento) {
+   // document.getElementById("mouse").innerHTML = "Passe o mouse aqui";
+   elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load () {
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value)
+
+}
